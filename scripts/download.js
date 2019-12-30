f5f464041930e516e71f95e41071df776f3dcf82
Sin <dev@zouxin.net>
2019-12-30 15:08:47 +0800

    feat: add prebuilt executable locally

--- scripts/download.js
+++ scripts/download.js
@@ -1,6 +1,42 @@
+const fs = require('fs')
+const path = require('path')
+const mkdirp = require('mkdirp')
 var downloader = require('../lib/downloader')
 
-downloader(function error (err, done) {
-  if (err) throw err;
-  console.log(done)
-})
+const deleteFolderRecursive = function (path) {
+  if (fs.existsSync(path)) {
+    fs.readdirSync(path).forEach(function (file, index) {
+      let curPath = path + '/' + file
+      if (fs.lstatSync(curPath).isDirectory()) {
+        deleteFolderRecursive(curPath)
+      } else {
+        fs.unlinkSync(curPath)
+      }
+    })
+    fs.rmdirSync(path)
+  }
+}
+
+const [, , ...flags] = process.argv
+const isWin =
+  flags.includes('--platform=windows') || require('../lib/util').isWin
+const executableName = isWin ? 'youtube-dl.exe' : 'youtube-dl'
+const detailsName = 'details'
+
+if (fs.existsSync(path.join(__dirname, '../prebuilt/', executableName))) {
+  mkdirp.sync(path.join(__dirname, '../bin/'))
+  fs.renameSync(
+    path.join(__dirname, '../prebuilt/', executableName),
+    path.join(__dirname, '../bin/', executableName)
+  )
+  fs.renameSync(
+    path.join(__dirname, '../prebuilt/', detailsName),
+    path.join(__dirname, '../bin/', detailsName)
+  )
+  deleteFolderRecursive(path.join(__dirname, '../prebuilt/'))
+} else {
+  downloader(function error (err, done) {
+    if (err) return console.log(err.stack)
+    console.log(done)
+  })
+}
